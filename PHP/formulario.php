f94ab68968c1fd6c1ca59db700e0311aa44aa4fb
gmuela <gmuela92@gmail.com>
2016-11-07 10:32:19 +0100

    Formulario + PHP terminado

--- PHP/formulario.php
+++ PHP/formulario.php
@@ -1,65 +1,80 @@
 <?php
 
-$errores=array();
+$errores = array();
 
-function validarFormulario(){
+function validarFormulario()
+{
 
     $formulario = $_POST;
+    $ficheros = $_FILES;
 
     global $errores;
 
-    $checked=true;
+    $checked = true;
 
-    if($formulario["nombre"]==null OR $formulario["email"]==null OR $formulario["pass"]==null OR $formulario["edad"]==null){
-        $checked=false;
+    if ($formulario["nombre"] == null OR $formulario["email"] == null OR $formulario["pass"] == null OR $formulario["edad"] == null) {
+        $checked = false;
         array_push($errores, "Los campos no pueden estar vacíos");
     }
 
-    if(strlen($formulario["pass"])<6){
-        $checked=false;
+    if (strlen($formulario["pass"]) < 6) {
+        $checked = false;
         array_push($errores, "La contraseña no puede tener menos de 6 caractéres");
     }
 
-    if($formulario["edad"]<1 OR $formulario["edad"]>120 OR is_nan($formulario["edad"])){
-        $checked=false;
+    if ($formulario["edad"] < 1 OR $formulario["edad"] > 120 OR is_nan($formulario["edad"])) {
+        $checked = false;
         array_push($errores, "Edad no válida(1-120)");
     }
 
-    if(strpos($formulario["email"],"@")==false){
-        $checked=false;
+    if (strpos($formulario["email"], "@") == false) {
+        $checked = false;
         array_push($errores, "Email no válido(xxx@xxxx)");
+    }
+
+    if ($ficheros["imagen"]["size"] > $formulario["MAX_FILE_SIZE"]) {
+        $checked = false;
+        array_push($errores, "La imagen supera el tamaño permitido(1MB)");
+    }
+    if ($ficheros["imagen"]["type"] != "image/jpeg") {
+        $checked = false;
+        array_push($errores, "La imagen no cumple con el formato permitido(.jpg)");
     }
 
     return $checked;
 
 }
 
-function mostrarDatos(){
+function mostrarDatos()
+{
     $formulario = $_POST;
+    $ficheros = $_FILES;
 
-    echo "Nombre: ".$formulario["nombre"]."<br>";
-    echo "Correo electrónico: ".$formulario["email"]."<br>";
-    echo "Contraseña: ".$formulario["pass"]."<br>";
-    echo "Edad: ".$formulario["edad"]."<br>";
+    move_uploaded_file($ficheros["imagen"]["tmp_name"], "./perfiles/" . $ficheros["imagen"]["name"]);
 
-    echo "Sexo: ".$formulario["sexo"]."<br>";
+    echo "Nombre: " . $formulario["nombre"] . "<br>";
+    echo "Correo electrónico: " . $formulario["email"] . "<br>";
+    echo "Contraseña: " . $formulario["pass"] . "<br>";
+    echo "Edad: " . $formulario["edad"] . "<br>";
+
+    echo "Sexo: " . $formulario["sexo"] . "<br>";
 
     echo "Intereses: ";
 
     foreach ($formulario["intereses"] as $interes) {
-        echo $interes.",";
+        echo $interes . ",";
     }
+
+    echo "Imagen de usuario: <img src='./perfiles/" . $ficheros["imagen"]["name"] . "'>";
 
     echo "<br>";
 }
 
-if(validarFormulario()){
+if (validarFormulario()) {
     echo "Te has registrado satisfactoriamente<br><br>";
     mostrarDatos();
-}
-
-else{
+} else {
     foreach ($errores as $error) {
-        echo $error."<br>";
+        echo $error . "<br>";
     }
 }
