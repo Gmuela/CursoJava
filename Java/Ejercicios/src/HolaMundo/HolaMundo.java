740969c21867e9a80d1f4a5b2f3e6e50db3cfa9d
gmuela <gmuela92@gmail.com>
2016-11-24 10:45:34 +0100

    Hola mundo

---
+++ Java/Ejercicios/src/HolaMundo/HolaMundo.java
@@ -0,0 +1,12 @@
+package HolaMundo;
+
+public class HolaMundo {
+
+    public static void main(String[] args){
+
+        for(String a: args) {
+            System.out.print(a+" ");
+        }
+    }
+
+}
