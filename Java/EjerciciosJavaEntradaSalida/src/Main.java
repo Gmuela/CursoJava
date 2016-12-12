e7b6326a19c550ae93ea63d258b24790d60e5d98
gmuela <gmuela92@gmail.com>
2016-12-12 13:33:02 +0100

    Añadido nuevo doc

---
+++ Java/EjerciciosJavaEntradaSalida/src/Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.io.Serializable;
+
+public class Main implements Serializable {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
